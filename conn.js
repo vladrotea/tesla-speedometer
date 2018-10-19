f3afd1d75a49090ea17fe90cbe6dd777c9ecd844
radu <cacinaradu@gmail.com>
2018-10-19 15:16:30 +0300

    sending long, lat via websocket

--- conn.js
+++ conn.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Starting connection');
     var ws = new WebSocket("ws://localhost:5678/");
+    // Connection opened
+    ws.addEventListener('open', function (event) {
+        ws.send('80, 100');
+    });
     ws.onmessage = function (event) {
         console.log('Data received ' + event.data);
         var data = JSON.parse(event.data);
