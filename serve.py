babe54dae28d735cdb88d876a58b210d6934549e
Vlad Rotea <vlad.rotea@fortech.ro>
2018-10-29 15:38:34 +0200

    use HTTPS for web server

--- serve.py
+++ serve.py
@@ -1,11 +1,18 @@
+import ssl
+import os
 import http.server
-import socketserver
 
-PORT = 8000
+REAL_PATH, _ = os.path.split(os.path.realpath(__file__))
+CERT_PATH = os.path.join(REAL_PATH, 'host.cert')
+KEY_PATH = os.path.join(REAL_PATH, 'host.key')
+
+SERVER_ADDRESS = ('0.0.0.0', 4443)
 
 Handler = http.server.SimpleHTTPRequestHandler
 
-httpd = socketserver.TCPServer(("", PORT), Handler)
+HTTPD = http.server.HTTPServer(SERVER_ADDRESS, Handler)
+HTTPD.socket = ssl.wrap_socket(HTTPD.socket, server_side=True, \
+        certfile=CERT_PATH, keyfile=KEY_PATH, ssl_version=ssl.PROTOCOL_TLSv1)
 
-print("serving at port", PORT)
-httpd.serve_forever()
+print("serving at: ", SERVER_ADDRESS)
+HTTPD.serve_forever()
